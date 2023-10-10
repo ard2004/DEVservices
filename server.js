238f3c1df37140462cff662727cd6b988cde88b8
captain-arvoy <accofstardust@gmail.com>
2023-10-10 23:28:59 +0530

    reloading tab

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT ?? 5001
 app.get('/',(req,res)=>{
-  res.sendFile(`/run/media/roshan/codebox/webWeek/index.html`)
+    // let time = parseInt((Date.now()/1000)%(Math.pow(10,1))) 
+    // res.setHeader('_differentiator_',(time%2==0))
+    res.setHeader("http-equiv","Refresh")
+    res.setHeader("content","60")
+    res.sendFile(`/run/media/roshan/codebox/webWeek/index.html`)
 })
 app.listen(PORT,()=>{
    console.log(`http://localhost:${PORT}`)   
